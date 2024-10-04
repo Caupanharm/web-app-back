6d9dc6c66109ebb81d09b8bc9e3d09e79a7669a5
Matéo Fontanel <fontanelmateo@gmail.com>
2024-10-04 09:05:45 +0200

    Deletion of deprecated code after features #1 and #5

--- src/main/kotlin/perso/caupanharm/backend/models/henrik/V1LifetimeMatches.kt
+++
@@ -1,9 +0,0 @@
-package perso.caupanharm.backend.models.henrik
-
-data class V1LifetimeMatches (
-    val status: String,
-    val name: String?,
-    val tag: String?,
-    val results: HenrikResults,
-    val data: List<HenrikV1LifetimeMatchesItem>
-)
