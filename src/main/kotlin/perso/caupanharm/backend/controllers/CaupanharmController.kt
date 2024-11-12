4778f19532de3e539fa2aeb030cac737e12258fb
Matéo Fontanel <fontanelmateo@gmail.com>
2024-11-12 07:54:53 +0100

    Add: fetched matches are now stored in db when possible to prevent reaching API rate limit too soon.

--- src/main/kotlin/perso/caupanharm/backend/controllers/CaupanharmController.kt
+++ src/main/kotlin/perso/caupanharm/backend/controllers/CaupanharmController.kt
@@ -4,21 +4,24 @@ import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 import perso.caupanharm.backend.repositories.MatchRepository
-import perso.caupanharm.backend.services.CaupanharmService
+import perso.caupanharm.backend.services.HenrikService
 import perso.caupanharm.backend.services.LocalDataService
 import perso.caupanharm.backend.models.caupanharm.CaupanharmResponse
+import perso.caupanharm.backend.models.caupanharm.CaupanharmResponseType
 import perso.caupanharm.backend.models.localdata.AdditionalCustomPlayerData
 import perso.caupanharm.backend.models.localdata.BracketMatchData
 import perso.caupanharm.backend.models.localdata.PlayersMatchData
 import perso.caupanharm.backend.models.caupanharm.valorant.match.full.CaupanharmMatchFull
+import perso.caupanharm.backend.models.caupanharm.valorant.matches.CaupanharmMatches
 import perso.caupanharm.backend.transformers.FullMatchTransformer
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @RestController
 @RequestMapping("/api")
 class CaupanharmController(
     private val localDataService: LocalDataService,
-    private val caupanharmService: CaupanharmService,
+    private val henrikService: HenrikService,
     private val fullMatchTransformer: FullMatchTransformer
 
 ) {
@@ -49,16 +52,59 @@ class CaupanharmController(
     fun getPlayer(@RequestParam username: String): Any {
         logger.info("Endpoint fetched: player with params: username=${username}")
         val splittedName = username.split('#')
-        return caupanharmService.getPlayerFromName(splittedName[0], splittedName[1])
+        return henrikService.getPlayerFromName(splittedName[0], splittedName[1])
 
     }
 
-    @GetMapping("/matches")
+    @GetMapping("matches")
     fun getStoredMatches(@RequestParam username: String): Mono<CaupanharmResponse> {
         logger.info("Endpoint fetched: matches with params: username=${username}")
         val splittedName = username.split('#')
-        return caupanharmService.getStoredMatches(splittedName[0], splittedName[1])
+
+        return henrikService.getStoredMatches(splittedName[0], splittedName[1])
+            .flatMap { response ->
+                val caupanharmMatches: MutableList<CaupanharmMatchFull> = repository.findByPlayerName(username)
+                    .map { it.toCaupanharmMatchFull() }
+                    .toMutableList()
+                logger.info(response.toString()                                                          )
+                if (response.bodyType == CaupanharmResponseType.MATCH_HISTORY) {
+                    val caupanharmMatchesIds: List<String> = caupanharmMatches.map { it.metadata.id }
+                    val allMatchesIds = (response.body as CaupanharmMatches).data.map { it.metadata.id }
+                    val missingMatchesIds = allMatchesIds.subtract(caupanharmMatchesIds.toSet())
+                    var matchesAdded = 0
+                    logger.info("Matches found: ${allMatchesIds.size}")
+                    logger.info("Matches stored: ${caupanharmMatchesIds.size}")
+
+                    // Utiliser un Flux pour récupérer tous les matchs manquants
+                    return@flatMap Flux.fromIterable(missingMatchesIds)
+                        .index()
+                        .flatMap { tuple ->
+                            val index = tuple.t1
+                            val matchId = tuple.t2
+                            henrikService.getMatchFromIdV4(matchId, missingMatchesIds.size, index.toInt() + 1)
+                                .filter { it.bodyType == CaupanharmResponseType.MATCH_FULL }
+                                .map { it.body as CaupanharmMatchFull }
+                                .doOnNext { match ->
+                                    try{
+                                        repository.save(match.toPostgresMatch())
+                                        matchesAdded++
+                                        caupanharmMatches.add(match)
+                                    }catch (e: Exception){
+                                        logger.error(e.stackTraceToString())
+                                    }
+                                }
+                        }
+                        .collectList() // Collecte tous les résultats dans une liste
+                        .flatMap { // Utiliser flatMap pour revenir à un Mono<CaupanharmResponse>
+                            Mono.just(CaupanharmResponse(200, "Added $matchesAdded matches to the database. ${missingMatchesIds.size - matchesAdded} matches are still missing due to rate limits. If matches are missing, try again in a minute.", CaupanharmResponseType.MATCH_HISTORY, caupanharmMatches))
+                        }
+                } else {
+                    // Si pas de MATCH_HISTORY, retourner la liste de départ dans un Mono
+                    return@flatMap Mono.just(CaupanharmResponse(500, "API Rate exceeded. Please try again in a minute.", CaupanharmResponseType.MATCH_HISTORY, caupanharmMatches))
+                }
+            }
     }
+
 
     @GetMapping("/match")
     fun getMatch(@RequestParam("id") matchId: String): Mono<CaupanharmResponse> {
@@ -71,12 +117,12 @@ class CaupanharmController(
                     CaupanharmResponse(
                         200,
                         null,
-                        bodyType = "exception",
+                        CaupanharmResponseType.EXCEPTION,
                         dbData
                     )
                 ) // TODO récupérer données db
             } else {
-                return caupanharmService.getMatchFromIdV4(matchId).map { response ->
+                return henrikService.getMatchFromIdV4(matchId).map { response ->
                     if (response.statusCode == 200) {
                         repository.save((response.body as CaupanharmMatchFull).toPostgresMatch())
                         response
@@ -86,7 +132,14 @@ class CaupanharmController(
                 }
             }
         } catch (e: Exception) {
-            return Mono.just(CaupanharmResponse(500, errorCode = null, bodyType = "exception", body = e.toString()))
+            return Mono.just(
+                CaupanharmResponse(
+                    500,
+                    null,
+                    CaupanharmResponseType.EXCEPTION,
+                    e.toString()
+                )
+            )
         }
     }
 
@@ -96,7 +149,7 @@ class CaupanharmController(
         @RequestParam("id") matchId: String
     ): Mono<CaupanharmResponse> {
         logger.info("Endpoint fetched: analysis with params: matchId=${matchId}")
-        val match = caupanharmService.getMatchFromIdV4(matchId)
+        val match = henrikService.getMatchFromIdV4(matchId)
         return match.flatMap { response ->
             if (response.statusCode == 200) {
                 fullMatchTransformer.analyseFullMatch(player, response.body as CaupanharmMatchFull)
--- src/main/kotlin/perso/caupanharm/backend/repositories/MatchRepository.kt
+++ src/main/kotlin/perso/caupanharm/backend/repositories/MatchRepository.kt
@@ -1,10 +1,24 @@
 package perso.caupanharm.backend.repositories
 
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import perso.caupanharm.backend.models.caupanharm.PostGresMatch
 
 @Repository
 interface MatchRepository : CrudRepository<PostGresMatch, String> {
+
+    // Recherche des matchs dont la colonne "players" contient un joueur avec un nom spécifique
+    @Query("""
+        SELECT m.*
+        FROM matches m
+        WHERE EXISTS (
+            SELECT 1
+            FROM jsonb_array_elements(m.players) AS player
+            WHERE player->>'name' ILIKE :playerName
+        )
+    """, nativeQuery = true)
+    fun findByPlayerName(playerName: String): List<PostGresMatch>
 
 }
