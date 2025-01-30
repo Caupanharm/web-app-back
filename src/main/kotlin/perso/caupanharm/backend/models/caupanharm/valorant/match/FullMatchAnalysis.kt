6979031fff2111dc2d1c23f185b735d200da06cb
Matéo Fontanel <fontanelmateo@gmail.com>
2025-01-30 02:40:27 +0100

    Added match impact analysis (without score)

--- src/main/kotlin/perso/caupanharm/backend/models/caupanharm/valorant/match/FullMatchAnalysis.kt
+++ src/main/kotlin/perso/caupanharm/backend/models/caupanharm/valorant/match/FullMatchAnalysis.kt
@@ -1,6 +1,80 @@
 package perso.caupanharm.backend.models.caupanharm.valorant.match
 
+import kotlin.math.roundToInt
+
 data class FullMatchAnalysis(
-    val attackRounds: List<CaupanharmMatchRound>,
-    val defenseRounds: List<CaupanharmMatchRound>
+    val rounds: List<CaupanharmMatchRound>, //TODO(remove after testing)
+    val global: SideAnalysis,
+    val attack: SideAnalysis,
+    val defense: SideAnalysis,
+)
+
+data class SideAnalysis(
+    val impact: ImpactAnalysis,
+    //val economy: EconomyAnalysis,
+    //val execution: ExecutionAnalysis,
+    //val strategy: StrategyAnalysis
+)
+
+interface Analyzable {
+    val computedScore: Double
+}
+
+data class ImpactAnalysis(
+    val rounds: Int,
+    val totalKills: Int,
+    val totalDeaths: Int,
+    val kd: Double,
+    val totalScore: Int,
+    val acs: Int,
+    val kastRounds: Int,
+    val averageKAST: Double,
+    val totalClutches: Int,
+    val wonClutches: Int,
+    val clutchWinRate: Double?,
+    val totalDuels: Int,
+    val wonDuels: Int,
+    val duelWinRate: Double?,
+): Analyzable{
+    override val computedScore: Double
+        get() = calculateScore()
+
+        private fun calculateScore(): Double {
+            return 0.0 // TODO("Not yet implemented")
+        }
+
+    operator fun plus(other: ImpactAnalysis): ImpactAnalysis{
+        return ImpactAnalysis(
+            this.rounds + other.rounds,
+            this.totalKills + other.totalKills,
+            this.totalDeaths + other.totalDeaths,
+            if(this.totalDeaths + other.totalDeaths == 0) (this.totalKills + other.totalKills).toDouble() else (this.totalKills + other.totalKills).toDouble() / (this.totalDeaths + other.totalDeaths),
+            this.totalScore + other.totalScore,
+            ((this.totalScore + other.totalScore).toDouble() / (this.rounds + other.rounds)).roundToInt(),
+            this.kastRounds + other.kastRounds,
+            (this.kastRounds + other.kastRounds).toDouble() / (this.rounds + other.rounds),
+            this.totalClutches + other.totalClutches,
+            this.wonClutches + other.wonClutches,
+            if(this.totalClutches + other.totalClutches == 0) null else (this.wonClutches + other.wonClutches).toDouble() / (this.totalClutches + other.totalClutches),
+            this.totalDuels + other.totalDuels,
+            this.wonDuels + other.wonDuels,
+             if(this.totalDuels + other.totalDuels == 0) null else (this.wonDuels + other.wonDuels).toDouble() / (this.totalDuels + other.totalDuels)
+        )
+    }
+}
+
+data class EconomyAnalysis(
+    val r1Econ: Boolean,
+    val r2Econ: Boolean,
+    val r3Econ: Boolean,
+    val lastRoundEcon: Boolean,
+    val teamEconMatching: Int
+)
+
+data class ExecutionAnalysis(
+    val placeholder: Any,
+)
+
+data class StrategyAnalysis(
+    val placeholder: Any,
 )
--- src/main/kotlin/perso/caupanharm/backend/services/MatchAnalysisService.kt
+++ src/main/kotlin/perso/caupanharm/backend/services/MatchAnalysisService.kt
@@ -4,11 +4,8 @@ import mu.KotlinLogging
 import org.springframework.stereotype.Component
 import perso.caupanharm.backend.models.caupanharm.CaupanharmResponse
 import perso.caupanharm.backend.models.caupanharm.CaupanharmResponseType
-import perso.caupanharm.backend.models.caupanharm.valorant.match.CaupanharmMatchFull
-import perso.caupanharm.backend.models.caupanharm.valorant.match.CaupanharmMatchRound
-import perso.caupanharm.backend.models.caupanharm.valorant.match.FullMatchAnalysis
+import perso.caupanharm.backend.models.caupanharm.valorant.match.*
 import perso.caupanharm.backend.utils.VALORANT_STRINGS
-import reactor.core.publisher.Mono
 
 private val logger = KotlinLogging.logger {}
 
@@ -39,15 +36,109 @@ class MatchAnalysisService {
             }
         }
 
+        logger.info("Analyzing ${match.rounds.size} rounds")
+        val attackImpactAnalysis = computeImpactAnalysis(playerId, attackRounds, match.kills)
+        val defenseImpactAnalysis = computeImpactAnalysis(playerId, defenseRounds, match.kills)
+        val totalImpactAnalysis = attackImpactAnalysis + defenseImpactAnalysis
 
-        /*
-        Process :
-        1. Split rounds into two lists, attack and defense
-        2. Run the same computations for both lists, without averaging anything
-        3. Output analysis data, both per side and globally
-         */
 
-        return CaupanharmResponse(200, null, CaupanharmResponseType.MATCH_ANALYSIS, FullMatchAnalysis(attackRounds, defenseRounds)) // TODO (placeholder return)
+        val response = FullMatchAnalysis(
+            rounds = match.rounds,
+            global = SideAnalysis(totalImpactAnalysis),
+            attack = SideAnalysis(attackImpactAnalysis),
+            defense = SideAnalysis(defenseImpactAnalysis)
+        )
 
+        return CaupanharmResponse(200, null, CaupanharmResponseType.MATCH_ANALYSIS, response)
+
+    }
+
+    fun computeImpactAnalysis(playerId: String, rounds: List<CaupanharmMatchRound>, kills: List<CaupanharmMatchKill>): ImpactAnalysis{
+        var totalKills = 0
+        var totalDeaths = 0
+        var totalScore = 0
+        var kastRounds = 0
+        var totalClutches = 0
+        var wonClutches = 0
+        var totalDuels = 0
+        var wonDuels = 0
+
+        for(round in rounds){
+            val playerStats = round.stats.single{ it.player == playerId }
+            val killEvents = kills.filter{ it.round == round.roundId }
+
+            totalKills += killEvents.filter{ it.killer == playerId }.size
+
+            totalDeaths += killEvents.filter{ it.victim == playerId }.size
+
+            totalScore += playerStats.score
+
+            if(hasKAST(playerId, killEvents)) kastRounds++
+
+            if(isClutchSituation()){
+                totalClutches++
+                if(didClutch()) wonClutches++
+            }
+
+            if(isDuelSituation()){
+                totalDuels++
+                if(didWinDuel()) wonDuels++
+            }
+
+        }
+
+        return ImpactAnalysis(
+            rounds = rounds.size,
+            totalKills = totalKills,
+            totalDeaths = totalDeaths,
+            kd = if(totalDeaths == 0) totalKills.toDouble() else totalKills.toDouble() / totalDeaths,
+            totalScore = totalScore,
+            acs = totalScore / rounds.size,
+            kastRounds = kastRounds,
+            averageKAST = kastRounds.toDouble() / rounds.size,
+            totalClutches = totalClutches,
+            wonClutches = wonClutches,
+            clutchWinRate = if(totalClutches == 0) null else wonClutches.toDouble() / totalClutches,
+            totalDuels = totalDuels,
+            wonDuels = wonDuels,
+            duelWinRate = if(totalDuels == 0) null else wonDuels.toDouble() / totalDuels
+        )
+    }
+
+    /*
+    KAST means getting a (K)ill, an (A)ssist, (S)urviving, or dying but getting (T)raded.
+    The definition of a trade is subjective, and we consider here that trading means killing someone's killer within 3 seconds of the initial kill.
+     */
+    fun hasKAST(playerId: String, killEvents: List<CaupanharmMatchKill>): Boolean{
+        if(killEvents.any{ it.killer == playerId }) return true
+        if(killEvents.any{ it.assistants.contains(playerId)}) return true
+
+        val deaths = killEvents.filter { it.victim == playerId } // One player can die multiple times in a round (Phoenix ult, Clove ult, Sage revive...)
+        if(deaths.isEmpty()) return true
+
+        for(death in deaths){
+            val killerId = death.killer
+            val playerDeathTime = death.roundTimeMillis
+            val killerDeaths = killEvents.filter { it.victim == killerId }
+            if(killerDeaths.find{ it.roundTimeMillis - playerDeathTime <= 3000} != null) return true
+        }
+
+        return false
+    }
+
+    fun isClutchSituation(): Boolean{
+        return false //TODO(Not yet implemented)
+    }
+
+    fun didClutch(): Boolean{
+        return false //TODO(Not yet implemented)
+    }
+
+    fun isDuelSituation(): Boolean{
+        return false //TODO(Not yet implemented)
+    }
+
+    fun didWinDuel(): Boolean{
+        return false //TODO(Not yet implemented)
     }
 }
